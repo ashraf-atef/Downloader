ff7e0300d5510da26f2a08e610595c8454ff6b6e
ashraf <ashrafatef84@gmail.com>
2015-08-14 18:11:11 +0100

    initial commit

---
+++ src/idm/Idm.java
@@ -0,0 +1,74 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package idm;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Ashraf
+ */
+public class Idm {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws MalformedURLException {
+        int a=2;
+        int b=3 ;
+       
+        System.out.println(a*100/b);
+        // TODO code application logic here
+        //download d=new download("http://localhost/sss.jpg","C:/Users/Boody/Desktop/photo_adel");
+//          download d1=new download("https://www.google.com/images/srpr/logo11w.png","C:/Users/Ashraf/Desktop/mybook1");
+//            download d2=new download("https://www.google.com/images/srpr/logo11w.png","C:/Users/Ashraf/Desktop/mybook2");
+       
+      //  Thread t=new Thread(d);
+//            Thread t1=new Thread(d1);
+//            Thread t2=new Thread(d2);
+            
+           // t.start();
+//            String ch=JOptionPane.showInputDialog(null, "enter");
+//            if (ch.endsWith("p"))
+//            {
+//                d.pauseThread();
+//            }
+//            
+//            
+//             ch=JOptionPane.showInputDialog(null, "enter");
+//            if (ch.endsWith("r"))
+//            {
+//                d.resumeThread();
+//            }
+//            List<Thread> threads = new ArrayList<>();
+//           threads.add(t);
+//           threads.get(0).start();
+//        try {
+//            threads.get(0).wait();
+//            
+//           
+//           
+//           
+//       
+////        try {
+////            d.downloadFromUrl(url, "book");
+////            System.out.println("file downloaded");
+////        } catch (IOException ex) {
+////            Logger.getLogger(Idm.class.getName()).log(Level.SEVERE, null, ex);
+////        }
+//  //  System.out.println(d.getFileSize(url));    
+//        } catch (InterruptedException ex) {
+//            Logger.getLogger(Idm.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+    }
+    
+}
